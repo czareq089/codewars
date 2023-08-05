846aabbe5b6d585d52b1528efe7eb166998f6419
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-06 01:00:07 +0200

    dodanie programu
    zamieniającego każdą literę w zdaniu na jej pozycję w alfabecie

---
+++ 05.08.23/Replace With Alphabet Position/Replace With Alphabet Position/Program.cs
@@ -0,0 +1,28 @@
+﻿char[] tekst = (Console.ReadLine()).ToArray();
+for (int i = 0; i < tekst.Length; i++)
+{
+    int pozycja = char.ToLower(tekst[i]) - 'a' + 1;
+    Console.Write(pozycja + " ");
+}
+
+public static class Kata
+{
+    public static string AlphabetPosition(string text)
+    {
+        char[] tekst = text.ToLower().ToCharArray();
+        string result = "";
+
+        for (int i = 0; i < tekst.Length; i++)
+        {
+            int pozycja = tekst[i] - 'a' + 1;
+            if (pozycja >= 1 && pozycja <= 26)
+            {
+                 result += pozycja + " ";
+            }
+
+
+        }
+
+        return result.Trim();
+    }
+}

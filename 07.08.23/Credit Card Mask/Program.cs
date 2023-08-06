63d518e56f0dd736d9d32258cb578695f75a823c
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-07 01:56:16 +0200

    dodanie programu
    maskującego wszystkie znaki oprócz czterech ostatnich podanego ciągu

---
+++ 07.08.23/Credit Card Mask/Program.cs
@@ -0,0 +1,26 @@
+﻿public static class Kata
+{
+    public static string Maskify(string cc)
+    {
+        char[] ccarray = cc.ToCharArray();
+        for (int i = 0; i < cc.Length - 4; i++)
+        {
+            if (cc[i] != '#')
+            {
+                ccarray[i] = '#';
+            }
+        }
+
+        cc = new string(ccarray);
+        return cc;
+    }
+
+    public static void Main(string[] args)
+    {
+        string cc = Console.ReadLine();
+        Console.WriteLine(Maskify(cc));
+    }
+}
+
+
+

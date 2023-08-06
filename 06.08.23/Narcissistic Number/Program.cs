e316db53c8b17d6c091cb6080860d5f0041ea218
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-06 23:50:52 +0200

    dodanie programu
    który sprawdza czy liczba jest narcystyczna (jej cyfry do potęgi o wartości długości po zsumowaniu dają oryginalną liczbę)

---
+++ 06.08.23/Narcissistic Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public static class Kata
+{
+    public static bool Narcissistic(int value)
+    {
+        string valueStr = value.ToString();
+        int length = valueStr.Length;
+        int sum = 0;
+
+        foreach (char c in valueStr)
+        {
+            int num = int.Parse(c.ToString());
+            sum += (int)Math.Pow(num, length);
+        }
+
+        return sum == value;
+    }
+    
+    public static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        bool isNarcissistic = Narcissistic(number);
+        Console.WriteLine(isNarcissistic);
+    }
+}
+

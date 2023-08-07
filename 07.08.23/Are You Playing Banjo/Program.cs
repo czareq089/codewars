762fcdac215c42e25ba45aa7b85f133d71837f03
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-07 23:43:33 +0200

    dodanie programu
    który sprawdza czy pierwsza litera podanego imienia to "R", wyświetlając odpowiedni komunikat w obu przypadkach

---
+++ 07.08.23/Are You Playing Banjo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Kata
+{
+    public static string AreYouPlayingBanjo(string name)
+    {
+        return name.ToLower()[0] == 'r' ? name + " plays banjo" : name + " does not play banjo";
+    }
+
+    public static void Main()
+    {
+        string name = Console.ReadLine();
+        Console.WriteLine(AreYouPlayingBanjo(name));
+    }
+}
+
+
+//// alternatywny warunek
+// if (name.ToLower()[0] == 'r')
+// {
+//     return name + " plays banjo";
+// }
+// else
+// {
+//     return name + " does not play banjo";

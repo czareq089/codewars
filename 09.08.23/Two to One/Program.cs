659c8318cf5e2cafd2d10773f00cf76373a8995e
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-09 18:06:26 +0200

    dodanie programu
    łączącego dwa słowa i oddającego w formie jednego posortowanego słowa złożonego z unikalnych liter

---
+++ 09.08.23/Two to One/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+public class TwoToOne 
+{
+  
+    public static string Longest (string s1, string s2)
+    {
+        string connected = string.Concat(s1, s2);
+        var dist = connected.Distinct();
+        string result = new string(dist.ToArray());
+        result = new string(result.OrderBy(c => c).ToArray());
+        return result;
+    }
+    public static void Main(string[] args)
+    {
+        string w1 = Console.ReadLine();
+        string w2 = Console.ReadLine();
+        Console.WriteLine(Longest(w1, w2));
+    }
+}

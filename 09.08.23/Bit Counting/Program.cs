f6d1d8a4b6d7030fe27ebc90753b52f708c73c1c
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-09 18:33:56 +0200

    dodanie programu
    zliczającego bity

---
+++ 09.08.23/Bit Counting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Kata
+{
+    public static int CountBits(int n)
+    {
+        string binary = Convert.ToString(n, 2);
+        int sum = 0;
+        for (int i = 0; i < binary.Length; i++)
+        {
+            if (binary[i] == '1')
+            {
+                sum++;
+            }
+        }
+
+        return sum;
+    }
+}
+

51cd1739e24b5e4ce46c0ce2040f2e02dfeb84cc
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-10 19:22:27 +0200

    dodanie programu
    którego wyjście to najmniejsza i największa liczba z podanego stringa

---
+++ 10.08.23/Highest and Lowest/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+public static class Kata
+{
+    public static string HighAndLow(string numbers)
+    {
+        var nums = numbers.Split(' ').Select(int.Parse).ToArray();
+        return $"{nums.Max()} {nums.Min()}";
+    }
+}

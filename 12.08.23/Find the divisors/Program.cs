386968043bab2d3a0568026073f17e91fa86132b
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-12 21:13:44 +0200

    dodanie programów

---
+++ 12.08.23/Find the divisors/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+public class Kata
+{
+    public static int[] Divisors(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                return n
+            }
+        }
+    }
+}

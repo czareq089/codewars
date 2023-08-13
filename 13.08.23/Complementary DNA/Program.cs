ba954b655222f15835534e8f860d4adfc80c17fb
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-13 19:47:43 +0200

    dodanie programów

---
+++ 13.08.23/Complementary DNA/Program.cs
@@ -0,0 +1,4 @@
+﻿public class DnaStrand
+{
+    public static string MakeComplement(string dna) => dna.Replace('A', 't').Replace('T', 'A').Replace('C', 'g').Replace('G', 'C').ToUpper();
+}
---
+++ 13.08.23/Exes and Ohs/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Linq;
+using System;
+public static class Kata
+{
+    public static bool XO(string input) => input.ToLower().Count(c => c == 'x') == input.ToLower().Count(c => c == 'o');
+}

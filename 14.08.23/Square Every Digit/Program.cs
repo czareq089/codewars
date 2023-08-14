1d49dce4f758d47c5acedbb5bda00b92c5f043eb
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-14 20:52:24 +0200

    dodanie programu

---
+++ 14.08.23/Square Every Digit/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int SquareDigits(int n) =>
+        int.Parse(string.Concat(n.ToString().Select(c => Math.Pow(int.Parse(c.ToString()), 2))));
+}

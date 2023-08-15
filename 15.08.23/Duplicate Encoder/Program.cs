15883115ba7220faed184efe555ec1c79e6cbd80
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-15 23:13:18 +0200

    dodanie programu
    nowe metody ogarnięte, na pewno się przydadzą

---
+++ 15.08.23/Duplicate Encoder/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Kata
+{
+    public static string DuplicateEncode(string word)
+    {
+        word = word.ToLower();
+        string result = "";
+        foreach (var c in word)
+        {
+            if (word.IndexOf(c) == word.LastIndexOf(c)) // IndexOf pierwszą zarejestrowaną oznacza pozycje litery c, LastIndex ostatnią, jeśli są równe to litera pojawiła się tylko raz 
+            {
+                result += "(";
+            }
+            else
+            {
+                result += ")";
+            }
+        }
+
+        return result;
+    }
+}

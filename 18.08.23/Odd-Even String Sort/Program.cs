269676fb37cf093007d2e9cd11e1468c9b62e3fc
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-18 13:42:04 +0200

    dodanie programu

---
+++ 18.08.23/Odd-Even String Sort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+public class Kata
+{
+    public static string SortMyString(string s)
+    {
+        string even = "";
+        string odd = "";
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i % 2 == 1)
+            {
+                odd += s[i];
+            }
+            else
+            {
+                even += s[i];
+            }
+        }
+
+        return even + " " + odd;
+    }
+}

e86de08b5761e583a15a2bfa51874e9bfc27437d
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-18 13:29:12 +0200

    dodanie programu

---
+++ 18.08.23/Calculate BMI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Kata
+{
+    public static string Bmi(double weight, double height)
+    {
+        var bmi = weight / Math.Pow(height, 2);
+        
+        switch (bmi)
+        {
+            case <=18.5:
+                return "Underweight";
+            case <=25:
+                return "Normal";
+            case <=30:
+                return "Overweight";
+            case >30:
+                return "Obese";
+        }
+
+        return bmi.ToString();
+    }
+}

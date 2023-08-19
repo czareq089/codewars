eae1b67518642303dfc1ebe8c1ae63ddd96545bd
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-19 19:29:29 +0200

    Dodanie programu

---
+++ 19.08.23/Operations with sequence/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int Calc(int[] array)
+    {
+        var squaredNumbers = array.Select(num => num > 0 ? num * num : num);
+        var multipliedThirdNumbers = squaredNumbers.Select((num, index) => (index + 1) % 3 == 0 ? num * 3 : num);
+        var multipliedFifthNumbers = multipliedThirdNumbers.Select((num, index) => (index + 1) % 5 == 0 ? num * -1 : num);
+        return multipliedFifthNumbers.Sum();
+    }
+}

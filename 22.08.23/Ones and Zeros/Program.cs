e0c6d0c7ea4a9ea7a38795106727473667330663
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2023-08-22 21:56:14 +0200

    dodanie programu

---
+++ 22.08.23/Ones and Zeros/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+namespace Solution
+{
+    class Kata
+    {
+        public static int binaryArrayToNumber(int[] BinaryArray) => Convert.ToInt32(string.Join("", BinaryArray));
+    }
+}
